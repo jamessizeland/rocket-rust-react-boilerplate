82bb1aaf2c57e579032f2baadccdc38b398e94fc
James Sizeland <jamessizeland@gmail.com>
2022-09-14 22:25:25 +0100

    change atom colours

--- src/components/Elements/Loader/atomicLoader.tsx
+++ src/components/Elements/Loader/atomicLoader.tsx
@@ -6,11 +6,13 @@ import './atom.css';
 export function AtomicLoader(): JSX.Element {
   return (
     <div className="justify-center flex p-6 m-auto">
-      <div className="la-ball-atom la-2x text-cyan-700">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="la-ball-atom la-2x">
+        <div className="satellite text-orange-300">
+          {/* <div className="crater border rounded-full bg-orange-400" /> */}
+        </div>
+        <div className="satellite text-red-600" />
+        <div className="satellite text-azure-600" />
+        <div className="satellite text-green-600" />
       </div>
     </div>
   );
